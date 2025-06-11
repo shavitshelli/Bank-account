fe8576fe1d39d18d48e11b88045fdbadf52be57b
shavit shelli <shavit.shelli59@gmail.com>
2025-06-11 11:20:09 +0300

    implemented savings_account.py

---
+++ bank/models/savings_account.py
@@ -0,0 +1,41 @@
+from account import Account,AccountType
+class SavingsAccount(Account):
+    def __init__(self, account_name, owner_id, initial_balance=0.0):
+        super().__init__(account_name, owner_id, initial_balance)
+        self.account_type = AccountType.SAVINGS
+        self.interest_rate = 0.02
+        self.minimum_balance = 1000
+        self.withdraw_limit_per_period = 3
+        self.withdraw_count = 0
+
+    
+    def withdraw(self, amount):
+        try:
+            super().withdraw(amount)
+        except ValueError as e:
+            raise e
+        if self.balance - amount < 0:
+            raise ValueError("Can't withdraw more than the balance")
+        if self.withdraw_count >= self.withdraw_limit_per_period:
+            raise ValueError("Can't withdraw more than the limit per period")
+        self.balance -= amount
+        self.transactions.append(f"withdraw:{amount}")
+        self.withdraw_count += 1
+
+    def get_account_type(self):
+        return AccountType.SAVINGS
+    
+    def get_interest_rate(self):
+        return self.interest_rate
+    
+    def add_interest(self):
+        if self.balance < self.minimum_balance:
+            raise ValueError("Can't add interest to an account with less than the minimum balance")
+        self.balance += self.balance * self.interest_rate
+        self.transactions.append(f"add_interest:{self.balance * self.interest_rate}")
+
+    def reset_withdraw_count(self):
+        self.withdraw_count = 0
+
+    def minimum_balance(self):
+        return self.minimum_balance
